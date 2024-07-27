b849c70961bd89211a7b335072ea94201df1aecd
sk-shahzad <shahzadd3v@gmail.com>
2024-07-27 06:00:49 -0700

    Added template and static file setting

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+# def loginpage(request):
+#     page = 'login'
+#     if request.user.authenticated:
+#         return redirect('home')
+#     if request.method == 'POST':
+#         username = request.POST.get('username').lower()
+#         password = request.POST.get('password')
+#         try:
+#             user = User.object.get(username=username)
+#         except :
+#             message.error(request, 'User does not exist')
+        
+#         user = authenticate(request, username=username, password=password)
+
+#         if user is not None:
+#             login(request, user)
+#             return redirect ('home')
+#         else: 
+#             message.error(request, 'Username or password does not exist')
+#             return render(request, 'login_register.html', {'page':page})
+
+
+        
--- salesawari/settings.py
+++ salesawari/settings.py
@@ -118,8 +118,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = 'static/'
+MEDIA_URL ='/images/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
